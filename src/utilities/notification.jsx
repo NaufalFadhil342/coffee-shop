982d49ab50596c4cf5b3b780a0a0e14668d2c6ec
Muhammad Naufal Fadhil Rahmat <naufalfadhil342@gmail.com>
2024-12-30 14:03:39 +0700

    update notification

--- src/utilities/notification.jsx
+++ src/utilities/notification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notification = ({ message }) => {
 
     return (
-        <div className='fixed w-[25rem] sm:w-3/4 h-auto top-28 right-10 bg-secondary px-4 py-2 rounded shadow-lg z-50 animate-slideIn'>
+        <div className='fixed w-2/3 sm:w-[25rem] h-auto top-28 right-10 bg-secondary px-4 py-2 rounded shadow-lg z-50 animate-slideIn'>
             <div className="flex flex-col items-start gap-4">
                 <p className='text-stone-800 font-medium'>{message}</p>
             </div>
